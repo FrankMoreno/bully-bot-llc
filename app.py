6c02d1b796a6bef2e4e16b3621676a39f403fe7d
Frank Moreno <Frank.Moreno95@gmail.com>
2018-03-20 00:32:40 -0500

    check for get method

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 import os
 import json
 
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def webserver():
     data = request.get_json()
-    if data['name'] != 'Bully':
-        createMessage(data)
+    #if data['name'] != 'Bully':
+    createMessage(data)
     return "ok", 200
 
 def createMessage(data):
-    postUrl = "https://api.groupme.com/v3/bots/post"
+    postUrl = 'https://api.groupme.com/v3/bots/post'
 
     payload = {
-                "bot_id" :  os.getenv('GROUPME_GOT_ID'),
-                "text" : "Shut up" + data['name']
+                'text' : "Shut up", #+ data['name']
+                'bot_id' :  os.getenv('GROUPME_GOT_ID'),
             }
 
-    request = Request(postUrl, urlencode(payload).encode())
-    json = urlopen(request).read().decode()
+    request = requests.post(postUrl, payload)
+    #json = urlopen(request).read().decode()
+
+@app.route('/', methods=['GET'])
+def simpleCheck():
+    request = requests.post("https://git.heroku.com/bully-bot-llc.git", "name:frank")
 
 
 
