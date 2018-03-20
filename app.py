fe948f942871a1518927d81f470f63d71f1f7717
Frank Moreno <Frank.Moreno95@gmail.com>
2018-03-20 00:09:35 -0500

    Simply response to messages in group

--- app.py
+++ app.py
@@ -7,30 +7,23 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-GROUPME_BOT_ID = os.getenv('GROUPME_GOT_ID')
-
 @app.route('/', methods=['POST'])
 def webserver():
     data = request.get_json()
-    data.dumps()
-
+    if data['name'] != 'Bully':
+        createMessage(data)
     return "ok", 200
 
-def createMessage():
-    {
-        "attachments": [],
-        "avatar_url": "",
-        "created_at": 1302623328,
-        "group_id": "1234567890",
-        "id": "1234567890",
-        "name": "John",
-        "sender_id": "12345",
-        "sender_type": "user",
-        "source_guid": "GUID",
-        "system": False,
-        "text": "Hello world ☃☃",
-        "user_id": "1234567890"
-}
+def createMessage(data):
+    postUrl = "https://api.groupme.com/v3/bots/post"
+
+    payload = {
+                "bot_id" :  os.getenv('GROUPME_GOT_ID'),
+                "text" : "Shut up" + data['name']
+            }
+
+    request = Request(postUrl, urlencode(payload).encode())
+    json = urlopen(request).read().decode()
 
 
 
